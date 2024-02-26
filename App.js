ff100ae2c398d5e942f64430fbe6fb8c9b2c6528
Saurav <rajeshwersoftsolution@gmail.com>
2024-02-26 18:39:24 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,22 @@
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable no-lone-blocks */
+/* eslint-disable comma-dangle */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable react/self-closing-comp */
+/* eslint-disable prettier/prettier */
+/* eslint-disable eol-last */
+//import liraries
+import React, { useState, useEffect } from 'react';
+import AppNavigator from './src/AppNavigator';
+
+const App = () =>
+{
+  
+
+
+
+
+  return <AppNavigator />;
+};
+
+export default App;
---
+++ Screens/CameraFullScreen.js
@@ -0,0 +1,33 @@
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable no-lone-blocks */
+/* eslint-disable comma-dangle */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable react/self-closing-comp */
+/* eslint-disable prettier/prettier */
+/* eslint-disable eol-last */
+//import liraries
+import React, {useState, useEffect} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+// create a component
+const CameraFullScreen = () => {
+const [counter,setCounter]=useState(0);
+const [someText,setSomeText]=useState('Hi');                                                                                                                                                                                                                                                                       return (
+    <View style={styles.container}>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+// define your styles
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#2c3e50',
+  },
+});
+
+//make this component available to the app
+export default CameraFullScreen;
